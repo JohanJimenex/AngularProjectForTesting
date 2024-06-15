e0d6360d5968eb1296693019c260c9fa3b6693f7
johanjimenex <johan.jimenez.23@gmail.com>
2024-06-15 16:29:32 -0400

    feat: Add unit tests

--- src/app/services/pokemon.service.spec.ts
+++ src/app/services/pokemon.service.spec.ts
@@ -1,18 +1,75 @@
 import { TestBed } from '@angular/core/testing';
 
-import { PokemonService } from './pokemon.service';
+import { PokemonOverviewRes, PokemonService } from './pokemon.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { PokemonDetails } from 'src/core/interfaces/pokemon-details.interface';
 
 describe('PokemonService', () => {
   let service: PokemonService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      
+      imports: [HttpClientTestingModule],
+      providers: [PokemonService],
     });
+
     service = TestBed.inject(PokemonService);
   });
+  it('should fetch pokemon overview list', () => {
+    const mockResponse: PokemonOverviewRes = {
+      results: [
+        { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+        { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+      ],
+    };
 
-  xit('should be created', () => {
-    expect(service).toBeTruthy(); 
+    service.getPokemonOverviewList(2, 0).subscribe((res) => {
+      expect(res).toEqual(mockResponse.results);
+    });
+  });
+
+  it('should fetch pokemon details', () => {
+    const mockResponse: PokemonDetails = {
+      abilities: [],
+      baseExperience: 0,
+      cries: {
+        latest: '',
+        legacy: '',
+      },
+      forms: [],
+      gameIndices: [],
+      height: 0,
+      heldItems: [],
+      id: 0,
+      isDefault: false,
+      locationAreaEncounters: '',
+      moves: [],
+      name: '',
+      order: 0,
+      pastAbilities: [],
+      pastTypes: [],
+      species: {
+        name: '',
+        url: '',
+      },
+      sprites: {
+        backDefault: '',
+        backFemale: null,
+        backShiny: '',
+        backShinyFemale: null,
+        frontDefault: '',
+        frontFemale: null,
+        frontShiny: '',
+        frontShinyFemale: null,
+      },
+      stats: [],
+      types: [],
+      weight: 0,
+    };
+
+    service.getPokemonDetails('bulbasaur').subscribe((res) => {
+      expect(res).toEqual(mockResponse);
+    });
   });
 });
