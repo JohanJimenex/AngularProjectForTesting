e316a9fb72460717a1fd5c0bd5d45f08cd3d84ea
jhoan_jimenez@bhdleon.com.do <jhoan_jimenez@bhdleon.com.do>
2024-06-20 22:34:05 -0400

    add new page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { TestComponent } from './pages/test/test.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomePageComponent,
+    // loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'test',
+    component: TestComponent,
     // loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
 
@@ -19,4 +25,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { TestComponent } from './pages/test/test.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent],
+  declarations: [AppComponent, HomePageComponent, TestComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
